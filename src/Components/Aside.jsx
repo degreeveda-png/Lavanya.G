7eebd24a65fd18cad209ad6a0908bd6041169e73
lavanya <degreeveda@gmail.com>
2026-02-13 20:35:50 -0800

    initial commit

--- src/Components/Aside.jsx
+++ src/Components/Aside.jsx
@@ -9,16 +9,21 @@ const Aside = () => {
 <div class="sun">
   <div class="bca">
  <h4> BCA</h4>
+ <div class="bt">3years</div>
  <strong>Bachelor in Computer Applications</strong>
 BCA course is a 3-year UG course that imparts knowledge on the basics of computer application and software development. BCA subjects include database management systems, software engineering, operating systems, web technology and computer languages such as HTML, C, C++, Java.
     </div>
     <div class="bca">
      <h4>BSC-DS</h4>
+      <div class="bt">3years</div>
+
     <strong> Data Science, Statistics, Computer Science</strong>
 B.Sc Data Science degree offers an extensive blend of Data Science, Statistics, Computer Science. B.Sc Data Science degree grants a true understanding of data programming, Statistical Analysis, and the usage and interpretation of data.
 </div>
 <div class="bca">
 <h4>BSC-CS</h4>
+ <div class="bt">3years</div>
+
 <strong>Mathematics, Chemistry, Computer Science
 </strong>
 
@@ -26,10 +31,14 @@ B.Sc Data Science degree offers an extensive blend of Data Science, Statistics, 
       </div>
       <div class="bca">
        <h4>BZC</h4>
+        <div class="bt">3years</div>
+
         <strong>  Botany, Zoology, Chemistry</strong>
        BSc BZC is a Bachelor's degree in science with Chemistry, Biology, and Zoology as a main subject. BSc BZC is a 3-year undergraduate program that can only be done after completing its 10+2 from a recognized board.
 </div>
 <div class="bca">
+   <div class="bt">3years</div>
+
 <h4>B.COM</h4>
 <strong>B.Com (Computer Applications)</strong>
 Bachelor of Commerce (Computer Applications) broadly covers the essentials and core disciplines of commerce like Economics, Finance, Business Law, etc. along with computer programming, hardware and software development and so on.
